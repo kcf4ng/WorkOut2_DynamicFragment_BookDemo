b89b19deb46cabb7c19774e7726380955cefc3d7
kcfang <kcf4ng@gmail.com>
2019-08-26 16:23:24 +0800

    Initial Commit

--- app/src/main/java/com/example/workout2/WorkoutDetailFragment.java
+++ app/src/main/java/com/example/workout2/WorkoutDetailFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -33,9 +34,25 @@ public class WorkoutDetailFragment extends Fragment {
        @Override
        public void onCreate(@Nullable Bundle savedInstanceState) {
            super.onCreate(savedInstanceState);
-           if (savedInstanceState != null){
+
+
+           if(savedInstanceState == null){
+               StopwatchFragment stopwatch = new StopwatchFragment();
+
+               FragmentTransaction ft = getChildFragmentManager().beginTransaction();
+
+               ft.add(R.id.stopwatch_container,stopwatch   );
+
+               ft.addToBackStack(null);
+
+               ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE  );
+
+               ft.commit();
+           }else{
                workoutId =savedInstanceState.getLong("workoutId");
            }
+
+
        }
 
        @Override
